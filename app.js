d0ae46deacb4437c503de45c1d7387a841c72a76
Julien Rouzot <rz.web.development@gmail.com>
2022-01-03 11:46:01 +0100

    [WIP] REST communication between services

--- app.js
+++ app.js
@@ -1,55 +1,83 @@
-const express = require("express");
+import fetch from "node-fetch"
+import express from "express"
+import FormData from "form-data"
 
-const HOST = "10.2.5.177";
-const PORT = 3000;
+const PORT = 50000
+
+const IP_GRID = "localhost"
+const PORT_GRID = 50001
+const ROUTE_GRID = "grid"
+
+const IP_DIGIT = "localhost"
+const PORT_DIGIT = 50002
+const ROUTE_DIGIT = "digit"
+
+const IP_SOLVER = "localhost"
+const PORT_SOLVER = 50003
+const ROUTE_SOLVER = "solver"
+
 
 // Create the express server instance
-const app = express();
+const app = express()
 
+// Allow to get body of a request
+app.use(express.json())
 
+// Test route
 app.get("/", (req, res) => {
-    console.log("request received");
-    res.send("ok");
-});
+    console.log("request received")
+    res.send("ok")
+})
 
 
 // Get a grid picture from the mobile app
-app.post("/grid", (req, res) => {
-    console.log(req.body);
-    res.json({
-        status: "success"
-    })
-});
+app.post("/grid", async (req, res) => {
+
+    console.log(req.body)
+
+    // The url of the grid recognition service
+    let url_grid = "http://" + IP_GRID + ":" + PORT_GRID + "/" + ROUTE_GRID
+    let url_digit = "http://" + IP_GRID + ":" + PORT_GRID + "/" + ROUTE_GRID
+    let url_solver = "http://" + IP_GRID + ":" + PORT_GRID + "/" + ROUTE_GRID
+
+    if(req.body) {
+
+        let formData = new FormData()
+        formData.append("photo_b64", req.body.image)
+
+        // GRID RECOGNITION
+        let response = await fetch(url_grid, {
+            method: "POST",
+            body: formData
+        });
+    
+        let json_grid =  await response.json()
+
+        // DIGIT RECOGNITION
+        let response = await fetch(url_digit, {
+            method: "POST",
+            body: formData
+        });
+    
+        let json_digit =  await response.json()
+
+        // SOLVER
+        let response = await fetch(url_solver, {
+            method: "POST",
+            body: formData
+        });
+    
+        let json_solver =  await response.json()
+
+    } else {
+
+        // TODO : ERROR HANDLING
+
+    }
+
+})
 
 
-// Get a grid structure from the grid recognition service
-app.post("/grid_recognition", (req, res) => {
-    console.log(req.body);
-    res.json({
-        status: "success"
-    })
-});
+app.listen(PORT)
 
-
-// Get the digits from the digit recognition service
-app.post("/digit_recognition", (req, res) => {
-    console.log(req.body);
-    res.json({
-        status: "success"
-    })
-});
-
-
-// Get the grid solved from the solver service
-app.post("/solver", (req, res) => {
-    console.log(req.body);
-    res.json({
-        status: "success"
-    })
-});
-
-
-
-app.listen(PORT, HOST);
-
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on port ${PORT}`)
